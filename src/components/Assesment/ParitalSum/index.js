8fd4a43faa3b86834a1c5e4d9de3f00a44be9ac3
Anuhya Gangireddy <101098221+Anuhya1002@users.noreply.github.com>
2022-10-27 01:13:57 +0530

    Update index.js

--- src/components/Assesment/ParitalSum/index.js
+++ src/components/Assesment/ParitalSum/index.js
@@ -5,12 +5,15 @@ import partialSums from './app.js'
 
     for (const x of input_array) {
         s += x;
-        return s;
+        return s;     
     }
+  if (s === sum){
+console.log(final_array)
 }
 
 const input_array = [4, 2, 5, 9, 12];
-console.log(Array.from(partialSums(input_array)).join(', '));
+const sum = 18;
+const final_array = Array.from(partialSums(input_array)).join(', ');
 
 
 export default partialSums
